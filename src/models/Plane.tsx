85cb5c4e799f325ce5df0e274f33d67752ea903a
ieva.kaleginiene <ieva.kaleginiene@gmail.com>
2023-12-01 22:12:51 +0200

    feat: added the plane moving around

---
+++ src/models/Plane.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useRef } from "react";
+import { useAnimations, useGLTF } from "@react-three/drei";
+
+import planeScene from "../assets/3d/plane.glb";
+import { ModelProps } from "./Island";
+
+const Plane: React.FC<ModelProps> = ({ isRotating, ...props }) => {
+  const ref = useRef();
+  const { scene, animations } = useGLTF(planeScene);
+  const { actions } = useAnimations(animations, ref);
+
+  useEffect(() => {
+    if (isRotating) {
+      actions["Take 001"]?.play();
+    } else {
+      actions["Take 001"]?.stop();
+    }
+  }, [actions, isRotating]);
+
+  return (
+    <mesh {...props} ref={ref}>
+      <primitive object={scene} />
+    </mesh>
+  );
+};
+
+export default Plane;
